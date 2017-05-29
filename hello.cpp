96443ea5e0a35764155b05490e910e9175dfd3f4
jinshaoxun <jinshaoxun@126.com>
2017-05-30 00:01:56 +0800

    本地仓库
    Change-Id: Ie5cf4c199dd75357a0f957f2adfd527200668a75
    Signed-off-by: jinshaoxun <jinshaoxun@126.com>

---
+++ hello.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+
+typedef struct
+{
+  int iNbr;
+  double dNbr;
+}stInformation;
+
+int main()
+{
+  cout<<"Hello, world !"<<endl;
+  cout<<"This is my first C++ example !"<<endl;
+
+  int iNbr = 10;
+  for(int i = 0; i < iNbr; ++i)
+  {
+    cout<<"i = "<<i;
+    cout<<"This is the master "<<endl;
+
+    cout<<""<<endl;
+
+//    printf("this is c format = %d\n", i);
+  }
+
+  return 0;
+}
